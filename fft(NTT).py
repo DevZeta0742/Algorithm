ccf214b96f1fb74c0f2794e147714c8f30f9f559
DevZeta <129294080+DevZeta0742@users.noreply.github.com>
2025-01-10 19:29:43 +0900

    Update and rename fft.py to fft(NTT).py

--- fft(NTT).py
+++ fft(NTT).py
@@ -57,45 +57,6 @@ def multiply_polynomials(A: list, B: list):  # 다항식 곱
 
     return c
 
-from math import ceil, log2
-
-p = 998244353
-
-def fft(a, inv):
-    n = len(a)
-    j = 0
-
-    for i in range(1, n):
-        rev = n // 2
-        while j >= rev:
-            j -= rev
-            rev //= 2
-        j += rev
-        if i < j:
-            a[i], a[j] = a[j], a[i]
-
-    step = 2
-    while step <= n:
-        half = step // 2
-        u = pow(3, p // step, p)
-        if inv:
-            u = pow(u, p - 2, p)
-
-        for i in range(0, n, step):
-            w = 1
-            for j in range(i, i + half):
-                v = a[j + half] * w % p
-                a[j + half] = (a[j] - v) % p
-                a[j] = (a[j] + v) % p
-                w = w * u % p
-
-        step *= 2
-
-    if inv:
-        num = pow(n, p - 2, p)
-        for i in range(n):
-            a[i] = a[i] * num % p
-
 def multiply(A: list, B: list):  # fft 활용해서 두 숫자 곱하는거긴한데 파이썬은 그냥 깡곱이 더 빠른듯
     n = 1
     total_length = len(str(A)) + len(str(B))
