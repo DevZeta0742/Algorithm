56198159c0150f155ac4806dbce5f3e5eac48f78
Atez_ <129294080+DevZeta0742@users.noreply.github.com>
2025-02-01 11:38:02 +0900

    Create LIS.py

---
+++ LIS.py
@@ -0,0 +1,25 @@
+import bisect
+
+def lcs(arr):  # nlogn
+    lst = []
+    for num in arr:
+        pos = bisect.bisect_left(lst, num)
+        if pos == len(lst):
+            lst.append(num)
+
+        else:
+            lst[pos] = num
+
+    return lst
+
+###################################################
+
+dp = [1] * n  # O(n^2)
+box = list(map(int, input().split()))
+
+for i in range(n):
+    for j in range(i):
+        if box[i] > box[j]:
+            dp[i] = max(dp[i], dp[j] + 1)
+        
+print(max(dp))
