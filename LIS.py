10589f1104e78b45dff1cb77e7ceca2bcd58619e
Atez_ <129294080+DevZeta0742@users.noreply.github.com>
2025-02-01 12:08:44 +0900

    Update LIS.py

--- LIS.py
+++ LIS.py
@@ -23,3 +23,44 @@ for i in range(n):
             dp[i] = max(dp[i], dp[j] + 1)
         
 print(max(dp))
+#                                                    lis length
+# ---------------------------------------------------
+#                                                    lis sequence
+import sys; input = sys.stdin.readline
+from bisect import bisect_left
+
+def LIS(seq): # nlogn
+    n = len(seq)
+    lst = []
+    pos = [0] * n
+    prev = [-1] * n
+
+    for i in range(n):
+        idx = bisect_left(lst, seq[i])
+        if idx == len(lst):
+            lst.append(seq[i])
+
+        else:
+            lst[idx] = seq[i]
+
+        pos[idx] = i
+        if idx > 0:
+            prev[i] = pos[idx - 1]
+
+    res = []
+    idx = pos[len(lst) - 1]
+    while idx != -1:
+        res.append(seq[idx])
+        idx = prev[idx]
+
+    res.reverse()
+
+    return res
+
+n = int(input())
+sequence = list(map(int,input().split()))
+
+ans = LIS(sequence)
+
+print(len(ans))
+print(*ans)
